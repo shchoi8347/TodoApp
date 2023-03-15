3b12ee3efb8b81a44d2eb088b3ea1a5f4d9cbb3b
= <=>
2023-03-15 13:53:57 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/dto/ResponseDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResponseDTO<T> {
+    private String error;
+    private List<T> data;
+}
+
+
+// new ResposeDTD<String>
+// new ResposeDTO<TodoDTO>
