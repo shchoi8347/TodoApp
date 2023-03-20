62395e69b1910eaeb318f7a830d75740cfaaefba
= <=>
2023-03-20 16:04:38 +0900

    3/20일 수업 완료

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
@@ -16,5 +19,43 @@ public class TestController {
     @GetMapping("/{id}")
     public String testControllerWithPathVariables(@PathVariable(required=false) int id) {
         return "Hello World, 최승훈" + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerWithRequestParam(@RequestParam(required=false) int id) {
+        return "Hello World, 최승훈" + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testControllerWithRequestParam(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "Hello World, 최승훈. id: " + testRequestBodyDTO.getId() + ", message: "
+                + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResposeBody() {
+        List<String> list = new ArrayList<>();
+        list.add("csh");
+        list.add("csh2");
+        list.add("csh3");
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+
+        return response;
+
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResposeEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("csh");
+        list.add("csh2");
+        list.add("csh3");
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+
+        return ResponseEntity.ok().body(response);
+        //return ResponseEntity.badRequest().body(response);
+
     }
 }
